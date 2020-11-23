de59f75a8995a863253dc978b81aaea9206c59b2
RicciCev <ricardo_christmann_apps1oa1920@cev.com>
2020-11-23 12:54:04 +0100

    added pipes

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ɵisDefaultChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     // selector es el nombre de este componente.
@@ -18,10 +18,23 @@ export class LoginComponent implements OnInit, OnDestroy {
 
     public user: string;
     public passw: string;
+    public validatePassw: boolean;
+
+    public arrayOfCars: Array<string>;
+
+    public dataCurrency: number = 456;
+    public dataDate = new Date(1996, 8, 20);
+    public dataDecimal: any = 500.678;
+    public dataJson = {foo: 'foo', goo: { too: 'new'}};
+    public dataString: string = 'Prueba';
+    public dataPercent: number = 580;
+    public dataSlice: Array<number> = [1, 2, 3, 4, 5, 6];
 
     constructor() {
         this.title = 'Este es nuestro login';
         this.titleLogin = true;
+        this.validatePassw = false;
+        this.arrayOfCars = ['ferrari', 'lambo', 'koeniggsegg', 'McLaren'];
     }
 
     // se ejecuta cuando se inicia esta página.
@@ -37,6 +50,12 @@ export class LoginComponent implements OnInit, OnDestroy {
     // método que gracias al databinding será llamado y ejecutado en la vista, ejemplo de databinding de vista a componente.
     public sendLogin(): void {
         console.log('El usuario es ' + this.user + ', la password es ' + this.passw);
+        // la passw debe contener almenos 6 caracteres.
+        if(this.passw.length < 6) {
+            this.validatePassw = false;
+        } else {
+            this.validatePassw = true;
+        }
     }
 
     public validateEmail(evento): void {
