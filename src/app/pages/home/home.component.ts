2da50c534d693bd8976b073237dc92c53683ad37
RicciCev <ricardo_christmann_apps1oa1920@cev.com>
2020-12-02 12:33:58 +0100

    added callback and promises in home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -13,9 +13,42 @@ export class HomeComponent implements OnInit {
 
     ContactModel = new ContactForm();
 
+    // EJEMPLO CALLBACK
+    asincroniaCallback = (list, callback) => {
+        // comprueba si list es de tipo array con el método instanceof, y la lista no debe estar vacía.
+        if (list instanceof Array && list.length > 0) {
+            /* con el método map manipulamos la lista, 'valor' será cada elemento de la lista,
+             y cada elemento será elevado a 2 con el método pow de la clase Math. */
+            const suma = list.map(valor => Math.pow(valor, 2));
+            // los callback siempre devuelven dos valores (error y respuesta), en este caso solo devolvemos la respuesta, que es la suma.
+            return callback(null, suma);
+        } else {
+            const error = new Error('Error de ejecución . . .');
+            // en este caso devuelve error y no una respuesta.
+            return callback(error, null);
+        }
+    }
+
+    // EJEMPLO PROMESA (devuelven un resolve para resultado o un reject para error)
+    asincroniaPromesa = (list) => {
+        const promise = new Promise((resolve, reject) => {
+            if (list instanceof Array && list.length > 0) {
+                const suma = list.map(valor => Math.pow(valor, 2));
+                resolve(suma);
+            } else {
+                const error = new Error('Error de ejecución . . .');
+                reject(error);
+            }
+        });
+        return promise;
+    }
+
     constructor(private homeService: HomeService) { }
 
-    ngOnInit(): void { }
+    ngOnInit(): void {
+        // en caso de que exista error mostrará por consola el error, y sino el resultado.
+        this.asincroniaCallback([1, 2, 3, 4, 5], (error, result) => (error) ? console.log(error) : console.log(result));
+    }
 
     onSubmit(f: NgForm) {
         this.homeService.sendFormContact(f);
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,11 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class HomeService {
+        public arrayDatos: Array<any>;
+    // siempre que veamos un array con '$' se tratará de un subject.
+    private listadoDatos$ = new Subject<Array<any>>();
+
+    constructor() {
+        this.arrayDatos = [];
+    }
+
     public sendFormContact(formulario: any) {
         console.log('El formulario ha sido enviado');
         console.log(formulario);
     }
+
+    agregarAListado(elemento: string) {
+        // agrega un elemento de tipo string a la lista arrayDatos.
+        this.arrayDatos.push(elemento);
+        // con esta línea el subject sabe que algo en el arrayDatos ha cambiado.
+        this.listadoDatos$.next(this.arrayDatos);
+    }
+
+    getDatos(): Observable<any> {
+        // este es el observable que estará observando si surgen cambios o no.
+        return this.listadoDatos$.asObservable();
+    }
+
+    /* devuelve un Observable ya que al hacer la petición get gracias al HttpClient,
+    estaremos escuchando hasta que la petición se complete y nos llegue los datos. */
+    /*
+    public getDataPhotos(): Observable<any> {
+        let url = AppEndPoints.ENDPOINTEJEMPLO;
+        return this.http.get(url);
+    }
+    */
 }
